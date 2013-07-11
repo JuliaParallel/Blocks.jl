99b425baf9d817f33ae4f115c65eaa97083bed61
tan <tanmaykm@gmail.com>
2013-07-11 15:21:31 +0530

    default to 1 step merge

--- test/filesort.jl
+++ test/filesort.jl
@@ -10,14 +10,14 @@ using Base.FS
 
 tmpf(workfile::String) = string(myid())*"_"*string(int(time_ns()))*"_"*workfile
 
-function sort_blocks(workfile::String, inio::Array) 
+function merge_blocks(workfile::String, inio::Array) 
     (length(inio) == 1) && return inio[1]
     out = tmpf(workfile)
-    sort_streams(open(out, "w"), map(x->open(x), inio)...)
+    merge_streams(open(out, "w"), map(x->open(x), inio)...)
     map(x->rm(x), inio)
     out
 end
-function sort_streams(out::IO, _inio::IO...)
+function merge_streams(out::IO, _inio::IO...)
     inio = [_inio...]
     nio = length(inio)
     lines = map(x->readline(x), inio)
@@ -35,16 +35,17 @@ function sort_streams(out::IO, _inio::IO...)
     return
 end
 
-function sort_step_2(workfile::String, block_files::Array)
+function sort_step_2(workfile::String, block_files::Array, nway::Int=0)
     # make pairs of files for pmap
-    npairs = int(ceil(length(block_files)/2))
+    (0 == nway) && (nway = length(block_files))
+    npairs = int(ceil(length(block_files)/nway))
     mb = cell(npairs)
     for idx in 1:npairs
         st = (idx-1)*2+1
         ed = min(length(block_files), st+1)
         mb[idx] = block_files[st:ed]
     end
-    block_files = pmap(x->sort_blocks(workfile, x), mb) 
+    block_files = pmap(x->merge_blocks(workfile, x), mb) 
     println("\tmerge -> $(length(block_files))...")
     #for fname in block_files
     #    println("\t$(fname)")
